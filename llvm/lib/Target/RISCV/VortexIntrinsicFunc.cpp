1c2f5cbe5f1c1a8a0e0c2ce8640cd2456c216cdc
Shinnung Jeong <sjeong306@gatech.edu>
2023-09-11 16:40:53 -0400

    minor bug fix

--- llvm/lib/Target/RISCV/VortexIntrinsicFunc.cpp
+++ llvm/lib/Target/RISCV/VortexIntrinsicFunc.cpp
@@ -135,6 +135,8 @@ bool VortexIntrinsicFuncLowering::runOnModule(Module &M) {
           continue;
         CallInst *CallInstr = dyn_cast<CallInst>(Instr);
         Function *Callee = CallInstr->getCalledFunction();
+        if (Callee == nullptr)
+          continue;
 
         int check = CheckFTarget(FTargets, Callee->getName());
 
